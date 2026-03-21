22a7762ce940590ae1ad6ab43356923cc8c8b9ed
Akash Salunkhe <akashsalunkhe232@nhitm.ac.in>
2026-03-21 18:45:40 +0530

    fix base url

--- pos-frontend/src/https/axiosWrapper.js
+++ pos-frontend/src/https/axiosWrapper.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const defaultHeader = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
-};
-
 export const axiosWrapper = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
-  withCredentials: true,
-  headers: { ...defaultHeader },
+  baseURL: "https://rest-vj63.onrender.com", // 🔥 YOUR BACKEND URL
+  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
