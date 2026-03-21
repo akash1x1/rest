8254411ba85186ed0b3dcf71e4fd60c636337308
Akash Salunkhe <akashsalunkhe232@nhitm.ac.in>
2026-03-21 15:11:35 +0530

    fixed render deployment

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -4,37 +4,39 @@ const config = require("./config/config");
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
 const app = express();
 
+const PORT = process.env.PORT || config.port;
 
-const PORT = config.port;
 connectDB();
 
 // Middlewares
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173']
-}))
-app.use(express.json()); // parse incoming request in json format
-app.use(cookieParser())
+    origin: true
+}));
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.set("trust proxy", 1);
 
 // Root Endpoint
 app.get("/", (req,res) => {
     res.json({message : "Hello from POS Server!"});
-})
+});
 
-// Other Endpoints
+// Routes
 app.use("/api/user", require("./routes/userRoute"));
 app.use("/api/order", require("./routes/orderRoute"));
 app.use("/api/table", require("./routes/tableRoute"));
 app.use("/api/payment", require("./routes/paymentRoute"));
 
-// Global Error Handler
+// Error Handler
 app.use(globalErrorHandler);
-
 
 // Server
 app.listen(PORT, () => {
-    console.log(`☑️  POS Server is listening on port ${PORT}`);
-})
+    console.log(`☑️ POS Server is listening on port ${PORT}`);
+});
